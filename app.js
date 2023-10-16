23f3b15bcf8ac4dbd9a1672aa9255bae43d11e60
rowanmorkner <56097720+rowanmorkner@users.noreply.github.com>
2023-10-15 19:10:02 -0700

    added previous and next buttons.
    the 'previous' button does not work correctly and wont update the index to '0'. it will skip and wrap around back to two.

--- app.js
+++ app.js
@@ -1,53 +1,73 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var data = { csvURL: window.location.origin + "/test.csv" };
+  let chart; // Declare the chart variable outside of the function for later reference
+  let index = 0;
 
-  let minValue = 0; // Calculate the minimum value
-  // let minValue = Math.min(...data); // Calculate the minimum value
+  let ds0 = [10, 5, 1];
+  let ds1 = [5, 5, 5];
+  let ds2 = [1, 5, 10];
+  let Data = [ds0, ds1, ds2];
 
-  Highcharts.chart("container", {
-    title: {
-      text: "Yield Curve",
-    },
-    subtitle: {
-      text: "Data from: 10/02/2023",
-    },
-    xAxis: {
-      categories: [
-        "1 Mo",
-        "2 Mo",
-        "3 Mo",
-        "4 Mo",
-        "6 Mo",
-        "1 Yr",
-        "2 Yr",
-        "3 Yr",
-        "5 Yr",
-        "7 Yr",
-        "10 Yr",
-        "20 Yr",
-        "30 Yr",
+  function drawChart() {
+    chart = Highcharts.chart("container", {
+      title: {
+        text: "Yield Curve",
+      },
+      subtitle: {
+        text: "Data from: 10/02/2023",
+      },
+      xAxis: {
+        categories: [
+          "1 Mo",
+          "2 Mo",
+          "3 Mo",
+          "4 Mo",
+          "6 Mo",
+          "1 Yr",
+          "2 Yr",
+          "3 Yr",
+          "5 Yr",
+          "7 Yr",
+          "10 Yr",
+          "20 Yr",
+          "30 Yr",
+        ],
+        title: {
+          text: "Maturity",
+        },
+      },
+      yAxis: {
+        title: {
+          text: "Yield (%)",
+        },
+        min: 0, // Static minimum for simplicity
+      },
+      series: [
+        {
+          name: "Yield",
+          data: Data[index],
+        },
       ],
-      title: {
-        text: "Maturity",
-      },
-    },
-    yAxis: {
-      title: {
-        text: "Yield (%)",
-      },
-      min: minValue, // Use the calculated minimum value
-    },
-    series: [
-      {
-        name: "Yield",
-        // data: { csv: csvData },
-        data: fakeData[index],
-      },
-    ],
+    });
+  }
+
+  // Initially draw the chart
+  drawChart();
+
+  // Handle the button click to toggle data
+  document.getElementById("jumpForward").addEventListener("click", function () {
+    // Increase the index and wrap around if necessary
+    index = (index + 1) % Data.length;
+    console.log("Current Data Index:", index);
+    // Update the series data
+    chart.series[0].setData(Data[index]);
+  });
+
+  document.getElementById("jumpBack").addEventListener("click", function () {
+    console.log("Back button clicked!"); // For testing
+    // Rest of your logic...
+    index = (index - 1 + Data.length) % Data.length;
+
+    // Update the series data
+    chart.series[0].setData(Data[index]);
   });
 });
-let index = 1;
-let ds1 = [
-  5.6, 5.61, 5.61, 5.63, 5.57, 5.37, 4.96, 4.74, 4.62, 4.66, 4.66, 5.03, 4.85,
-];
-let fakeData = [[1, 2, 3], ds1, [7, 6, 8]];
